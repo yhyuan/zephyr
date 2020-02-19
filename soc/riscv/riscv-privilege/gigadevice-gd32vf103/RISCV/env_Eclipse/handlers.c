9a3a26f47d262cd0228fecf3220de7ca1bbc019e
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-02-20 07:59:11 +0900

    warn

--- soc/riscv/riscv-privilege/gigadevice-gd32vf103/RISCV/env_Eclipse/handlers.c
+++ soc/riscv/riscv-privilege/gigadevice-gd32vf103/RISCV/env_Eclipse/handlers.c
@@ -8,7 +8,7 @@
 __attribute__((weak)) uintptr_t handle_nmi()
 {
   //write(1, "nmi\n", 5);
-  _exit(1);
+  //_exit(1);
   return 0;
 }
 
@@ -22,7 +22,7 @@ __attribute__((weak)) uintptr_t handle_trap(uintptr_t mcause, uintptr_t sp)
   //printf("In trap handler, the mcause is %d\n", mcause);
   //printf("In trap handler, the mepc is 0x%x\n", read_csr(mepc));
   //printf("In trap handler, the mtval is 0x%x\n", read_csr(mbadaddr));
-  _exit(mcause);
+  //_exit(mcause);
   return 0;
 }
 
