4f78fd3c3dfb7818ac2204e9b19d2c47ca37505a
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-03-03 21:27:10 +0900

    [riscv] Add configuration option for specify exception mask
    Some core uses mcause register for storing information
    additionally to exception code.
    Add configuration for exception code bits mask
    to make able to handle these information.

--- soc/riscv/riscv-privilege/common/soc_common.h
+++ soc/riscv/riscv-privilege/common/soc_common.h
@@ -25,15 +25,14 @@
 /* Interrupt Mask */
 #define SOC_MCAUSE_IRQ_MASK          (1 << 63)
 /* Exception code Mask */
-#define SOC_MCAUSE_EXP_MASK          0x7FFFFFFFFFFFFFFF
 #else
 /* Interrupt Mask */
 #define SOC_MCAUSE_IRQ_MASK          (1 << 31)
 /* Exception code Mask */
-#define SOC_MCAUSE_EXP_MASK          0x7FFFFFFF
 #endif
 /* ECALL exception number */
 #define SOC_MCAUSE_ECALL_EXP         RISCV_MACHINE_ECALL_EXP
+#define SOC_MCAUSE_EXP_MASK          CONFIG_RISCV_SOC_MCAUSE_EXCEPTION_MASK
 
 /* SOC-Specific EXIT ISR command */
 #define SOC_ERET                     mret
