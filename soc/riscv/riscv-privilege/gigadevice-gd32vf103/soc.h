313d5c140d8ec5d68b1349e24b809f52752e16c5
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-03-04 21:40:01 +0900

    nuclei_eclic

--- soc/riscv/riscv-privilege/gigadevice-gd32vf103/soc.h
+++ soc/riscv/riscv-privilege/gigadevice-gd32vf103/soc.h
@@ -46,4 +46,20 @@
 #define RISCV_RAM_BASE               DT_SRAM_BASE_ADDRESS
 #define RISCV_RAM_SIZE               KB(DT_SRAM_SIZE)
 
+#ifndef _ASMLANGUAGE
+#include "gd32vf103.h"
+#include "n200_func.h"
+
+#define ARCH_IRQ_CONNECT(irq_p, priority_p, isr_p, isr_param_p, flags_p) \
+({ \
+	Z_ISR_DECLARE(irq_p, 0, isr_p, isr_param_p); \
+	eclic_set_nonvmode(irq_p); \
+	eclic_set_level_trig(irq_p); \
+	eclic_set_irq_lvl_abs(irq_p, 1); \
+	arch_irq_priority_set(irq_p, priority_p); \
+	irq_p; \
+})
+
+#endif /* !_ASMLANGUAGE */
+
 #endif /* __RISCV_GIGADEVICE_GD32VF103_SOC_H_ */
