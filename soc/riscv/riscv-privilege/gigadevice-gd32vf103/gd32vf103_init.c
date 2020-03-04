15d8a58b3cc3af9eb30836974239c7e326d5b3c2
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-03-04 22:13:48 +0900

    eclic

--- soc/riscv/riscv-privilege/gigadevice-gd32vf103/gd32vf103_init.c
+++ soc/riscv/riscv-privilege/gigadevice-gd32vf103/gd32vf103_init.c
@@ -11,23 +11,6 @@ static int _init(struct device* dev)
 
 	SystemInit();
 
-	//ECLIC init
-	eclic_init(ECLIC_NUM_INTERRUPTS);
-	eclic_mode_enable();
-
-
-    eclic_global_interrupt_enable();
-
-
-	//printf("After ECLIC mode enabled, the mtvec value is %x \n\n\r", read_csr(mtvec));
-
-	// // It must be NOTED:
-	//  //    * In the RISC-V arch, if user mode and PMP supported, then by default if PMP is not configured
-	//  //      with valid entries, then user mode cannot access any memory, and cannot execute any instructions.
-	//  //    * So if switch to user-mode and still want to continue, then you must configure PMP first
-	//pmp_open_all_space();
-	//switch_m2u_mode();
-	
     /* Before enter into main, add the cycle/instret disable by default to save power,
     only use them when needed to measure the cycle/instret */
 	__asm__("csrsi " STRINGIFY(CSR_MCOUNTINHIBIT) ",0x5");
